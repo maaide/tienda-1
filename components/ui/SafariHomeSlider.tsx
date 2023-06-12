39fbf44233030056a441e27b8689d16cedf07a2b
Jorge <admin@maaide.com>
2023-06-12 10:36:03 -0400

    actualization general

--- components/ui/SafariHomeSlider.tsx
+++ components/ui/SafariHomeSlider.tsx
@@ -9,21 +9,20 @@ import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper"
 
 export const SafariHomeSlider = () => {
 
+  const [loadImage, setLoadImage] = useState(false)
   const [imageView, setImageView] = useState('opacity-0')
   const [textView, setTextView] = useState('opacity-0')
   const [buttonView, setButtonView] = useState('opacity-0')
 
   useEffect(() => {
+    setImageView('opacity-1')
     setTimeout(() => {
-      setImageView('opacity-1')
+      setTextView('opacity-1')
       setTimeout(() => {
-        setTextView('opacity-1')
-        setTimeout(() => {
-          setButtonView('opacity-1')
-        }, 250)
+        setButtonView('opacity-1')
       }, 300)
-    }, 200)
-  }, [])
+    }, 300)
+  }, [loadImage])
 
   return (
     <Swiper
@@ -37,21 +36,23 @@ export const SafariHomeSlider = () => {
       modules={[Navigation, Pagination, Mousewheel, Keyboard]}
     >
       <SwiperSlide>
-        <div className={`${imageView} h-400 flex p-4 transition-opacity duration-200 bg-cover bg-[url(https://res.cloudinary.com/blasspod/image/upload/v1680628242/blaspod/pexels-xue-guangjian-1687845_ocry6n.jpg)] xl:h-600 2xl:h-700`}>
+        <div className={`h-400 flex xl:h-600 2xl:h-700`}>
           <div className='w-1280 m-auto'>
             <h1 className={`${textView} transition-opacity duration-150 text-[25px] text-white font-bold mb-2 md:text-[32px]`}>ENCUÉNTRA OFERTAS DE HASTA UN 40% DE DESCUENTO</h1>
             <p className={`${textView} transition-opacity duration-150 text-white text-lg mb-4`}>Aprovecha nuestras increibles ofertas solo por tiempo limitado.</p>
             <Link className={`${buttonView} transition-opacity duration-150`} href='/ofertas'><Button>VER OFERTAS</Button></Link>
           </div>
+          <img onLoad={() => setLoadImage(true)} className={`absolute -z-10 ${imageView} transition-opacity duration-200`} src='https://res.cloudinary.com/blasspod/image/upload/v1680628242/blaspod/pexels-xue-guangjian-1687845_ocry6n.jpg' />
         </div>
       </SwiperSlide>
       <SwiperSlide>
-        <div className='h-400 flex p-4 bg-cover bg-[url(https://res.cloudinary.com/blasspod/image/upload/v1680628242/blaspod/pexels-xue-guangjian-1687845_ocry6n.jpg)] xl:h-600 2xl:h-700'>
+        <div className='h-400 flex xl:h-600 2xl:h-700'>
           <div className='w-1280 m-auto'>
             <h1 className='text-[25px] text-white font-bold mb-2 md:text-[32px]'>ENCUÉNTRA OFERTAS DE HASTA UN 40% DE DESCUENTO</h1>
             <p className='text-white text-lg mb-4'>Aprovecha nuestras increibles ofertas solo por tiempo limitado.</p>
             <Button>VER OFERTAS</Button>
           </div>
+          <img onLoad={() => setLoadImage(true)} className={`absolute -z-10`} src='https://res.cloudinary.com/blasspod/image/upload/v1680628242/blaspod/pexels-xue-guangjian-1687845_ocry6n.jpg' />
         </div>
       </SwiperSlide>
     </Swiper>
