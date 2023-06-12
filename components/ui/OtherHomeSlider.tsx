a1ea4017c3357b4ac17f349464ecb8bd815bfe84
Jorge <admin@maaide.com>
2023-06-12 10:05:08 -0400

    actualization general

--- components/ui/OtherHomeSlider.tsx
+++ components/ui/OtherHomeSlider.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from './Button'
 import Link from 'next/link'
 import { Swiper, SwiperSlide } from "swiper/react"
@@ -8,6 +8,23 @@ import styles from "./css/OtherHomeSlider.module.css"
 import { Navigation, Pagination } from "swiper"
 
 export const OtherHomeSlider = () => {
+
+  const [imageView, setImageView] = useState('opacity-0')
+  const [textView, setTextView] = useState('opacity-0')
+  const [buttonView, setButtonView] = useState('opacity-0')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setImageView('opacity-1')
+      setTimeout(() => {
+        setTextView('opacity-1')
+        setTimeout(() => {
+          setButtonView('opacity-1')
+        }, 250)
+      }, 300)
+    }, 200)
+  }, [])
+
   return (
     <Swiper
       className={styles.mySwiper}
@@ -19,11 +36,11 @@ export const OtherHomeSlider = () => {
       modules={[Pagination, Navigation]}
     >
       <SwiperSlide>
-        <div className='h-400 flex p-4 bg-cover bg-[url(https://res.cloudinary.com/blasspod/image/upload/v1680628242/blaspod/pexels-xue-guangjian-1687845_ocry6n.jpg)] xl:h-600 2xl:h-700'>
+        <div className={`${imageView} h-400 transition-opacity duration-200 flex p-4 bg-cover bg-[url(https://res.cloudinary.com/blasspod/image/upload/v1680628242/blaspod/pexels-xue-guangjian-1687845_ocry6n.jpg)] xl:h-600 2xl:h-700`}>
           <div className='w-1280 m-auto'>
-            <h1 className='text-[25px] text-white font-bold mb-2 md:text-[32px]'>ENCUÉNTRA OFERTAS DE HASTA UN 40% DE DESCUENTO</h1>
-            <p className='text-white text-lg mb-4'>Aprovecha nuestras increibles ofertas solo por tiempo limitado.</p>
-            <Link href='/ofertas'><Button>VER OFERTAS</Button></Link>
+            <h1 className={`${textView} text-[25px] transition-opacity duration-150 text-white font-bold mb-2 md:text-[32px]`}>ENCUÉNTRA OFERTAS DE HASTA UN 40% DE DESCUENTO</h1>
+            <p className={`${textView} text-white transition-opacity duration-150 text-lg mb-4`}>Aprovecha nuestras increibles ofertas solo por tiempo limitado.</p>
+            <Link className={`${buttonView} transition-opacity duration-150`} href='/ofertas'><Button>VER OFERTAS</Button></Link>
           </div>
         </div>
       </SwiperSlide>
