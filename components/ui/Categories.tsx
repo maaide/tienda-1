d93c99a9bb6ff060611d6e4eb42056d254d2ebf8
Jorge <admin@maaide.com>
2023-06-12 22:31:47 -0400

    actualization general

--- components/ui/Categories.tsx
+++ components/ui/Categories.tsx
@@ -36,7 +36,7 @@ export const Categories: React.FC<Props> = ({ categories }) => {
                 {
                   categories.map(category => (
                     <Link href={`/tienda/${category.slug}`} key={category._id} className='mb-2 p-1 bg-contain w-full flex gap-4 bg-center hover:opacity-70 830:block 830:w-96'>
-                      <Image className={`${imgView} transition-opacity duration-200 w-1/2 830:w-96`} onLoadingComplete={() => setImgLoad(true)} src={category.image!} alt={category.category} />
+                      <Image className={`${imgView} transition-opacity duration-200 w-1/2 830:w-96`} width={500} height={500} onLoadingComplete={() => setImgLoad(true)} src={category.image!} alt={category.category} />
                       <div className='m-auto'>
                         <h2 style={{ display: imgLoad ? 'block' : 'none' }} className={`${textView} transition-opacity duration-200 text-[16px] mb-1 text-[#1c1b1b] tracking-widest font-semibold mt-2 md:text-[20px] dark:text-white`}>{category.category.toUpperCase()}</h2>
                         <p className={`${textView} transition-opacity duration-200 text-sm text-main md:text-base dark:text-neutral-400`}>{category.description}</p>
