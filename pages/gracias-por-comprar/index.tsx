c60f9fbda142c8ac9d7f3dab7f3541e79eb50cdc
Jorge <admin@maaide.com>
2023-06-29 13:56:08 -0400

    actualization general

--- pages/gracias-por-comprar/index.tsx
+++ pages/gracias-por-comprar/index.tsx
@@ -1,8 +1,13 @@
+import { Spinner } from '@/components/ui'
 import React from 'react'
 
 const PageBuySuccess = () => {
   return (
-    <div>PageBuySuccess</div>
+    <div className='w-full h-full bg-white fixed flex'>
+      <div className='w-fit m-auto'>
+        <Spinner />
+      </div>
+    </div>
   )
 }
 
