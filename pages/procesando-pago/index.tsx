d14500cc2e61fa710b49e3eec7947d4b844ef634
Jorge <admin@maaide.com>
2023-06-29 13:51:52 -0400

    actualization general

--- pages/procesando-pago/index.tsx
+++ pages/procesando-pago/index.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 
 const PayProcess = () => {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const { token_ws } = router.query
+    if (token_ws) {
+        router.push('/gracias-por-comprar')
+    }
+  }, [])
+
   return (
     <div>PayProcess</div>
   )
