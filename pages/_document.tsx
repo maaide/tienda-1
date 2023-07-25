d8f94ded182b5bce157947dc49a7bd6d9d5c6f4e
Jorge <admin@maaide.com>
2023-07-25 11:57:00 -0400

    actualization general

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-PHWFK32XKT"></script>
+        <script
+          dangerouslySetInnerHTML={{
+             __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PHWFK32XKT');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
